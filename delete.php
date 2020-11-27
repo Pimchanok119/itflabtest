ad3443bef887e82e05e614133f098238175129ed
Pimchanok119 <73678432+Pimchanok119@users.noreply.github.com>
2020-11-27 10:55:13 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,10 +1,10 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'moopimisallaround.mysql.database.azure.com', 'moopim@moopimisallaround', 'PPNuro26', 'itflab', 3306);
+mysqli_real_connect($conn, 'moopimisallaround.mysql.database.azure.com', 'moopim@moopimisallaround' , 'PPNuro26', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
-    die('Failed to connect to MySQL PLEASE TRY AGAIN!: '.mysqli_connect_error());
+    die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
 }
 
 $id = $_GET['id'];
@@ -20,4 +20,3 @@ if (mysqli_query($conn, $sql)) {
   
 mysqli_close($conn);
 ?>
-
