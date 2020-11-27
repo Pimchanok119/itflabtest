eae26d5c9fcbbed490901ba34471e8d7617c4a1e
Pimchanok119 <73678432+Pimchanok119@users.noreply.github.com>
2020-11-27 10:41:20 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,21 +1,22 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'moopimisallaround.mysql.database.azure.com', 'moopim@moopimisallaround' , 'PPNuro26', 'itflab', 3306);
+mysqli_real_connect($conn, 'moopimisallaround.mysql.database.azure.com', 'moopim@moopimisallaround', 'PPNuro26', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL PLEASE TRY AGAIN!: '.mysqli_connect_error());
 }
 
+// $id = $_GET['id];
+//$sql = 'DELETE FROM guestbook WHERE id = $ id';
+$name = $_POST['name'];
+$comment = $_POST['comment'];
 
-$ID = $_POST['ID'];
-
-
-$sql = "DELETE FROM guestbook WHERE id='$ID'";
-
+//$sql = $sql = "DELETE FROM guestbook WHERE name='$name'";
+$sql = "DELETE FROM guestbook WHERE Comment='$comment'";
 
 if (mysqli_query($conn, $sql)) {
-    header("Location: show.php");
+    header('Location: https://phpscript52.azurewebsites.net/show.php');
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
