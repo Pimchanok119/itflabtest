50bb49ca4ebe984f3c0ca4ebcf1c478ecefe0ff7
Pimchanok119 <73678432+Pimchanok119@users.noreply.github.com>
2020-11-27 10:47:03 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -7,16 +7,13 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL PLEASE TRY AGAIN!: '.mysqli_connect_error());
 }
 
-// $id = $_GET['id];
-//$sql = 'DELETE FROM guestbook WHERE id = $ id';
-$name = $_POST['name'];
-$comment = $_POST['comment'];
+$id = $_GET['id'];
+$sql = "DELETE FROM guestbook WHERE id = '$id'";
 
-//$sql = $sql = "DELETE FROM guestbook WHERE name='$name'";
-$sql = "DELETE FROM guestbook WHERE Comment='$comment'";
+
 
 if (mysqli_query($conn, $sql)) {
-    header('Location: https://phpscript52.azurewebsites.net/show.php');
+    echo "Success " . $sql . "<br>" . mysqli_error($conn);
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
